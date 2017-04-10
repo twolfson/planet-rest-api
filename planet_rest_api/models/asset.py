e0aabc5f4d11cfafdee413be6521630dd174fef0
Todd Wolfson <todd@twolfson.com>
2017-04-10 13:48:15 -0700

    Added thoughts for validation

--- planet_rest_api/models/asset.py
+++ planet_rest_api/models/asset.py
@@ -1,13 +1,26 @@
 # Define a store for all assets
 asset_map = {}
 
+# TODO: Disallow overwriting existing assets
+# TODO: Unit test bad names and spy our create method uses validate
+
+
 # Define our Asset class
 class Asset(object):
     def __init__(self, name):
+        # Save our model information
         self.name = name
 
     def save(self):
+        # Run validation
+        self.validate()
+
+        # Save our model
         asset_map[self.name] = self
+
+    def validate(self):
+        # Validate our asset name
+        pass
 
     def serialize(self):
         return {
